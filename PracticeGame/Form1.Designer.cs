b6bc48b2946c7636d49ef5d8e7707e7862e3aa36
mayaekie071 <mayaekie071@SDS-L-191HC63>
2024-09-11 11:18:40 -0400

    Add project files.

---
+++ PracticeGame/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PracticeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startbutton = new System.Windows.Forms.Button();
+            this.gametitle = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startbutton
+            // 
+            this.startbutton.BackColor = System.Drawing.Color.Black;
+            this.startbutton.Font = new System.Drawing.Font("Courier New", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startbutton.ForeColor = System.Drawing.SystemColors.Info;
+            this.startbutton.Location = new System.Drawing.Point(353, 176);
+            this.startbutton.Name = "startbutton";
+            this.startbutton.Size = new System.Drawing.Size(107, 68);
+            this.startbutton.TabIndex = 0;
+            this.startbutton.Text = "Start";
+            this.startbutton.UseVisualStyleBackColor = false;
+            this.startbutton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // gametitle
+            // 
+            this.gametitle.BackColor = System.Drawing.Color.Black;
+            this.gametitle.Font = new System.Drawing.Font("Courier New", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gametitle.ForeColor = System.Drawing.SystemColors.Info;
+            this.gametitle.Location = new System.Drawing.Point(329, 124);
+            this.gametitle.Name = "gametitle";
+            this.gametitle.Size = new System.Drawing.Size(148, 30);
+            this.gametitle.TabIndex = 1;
+            this.gametitle.Text = "Insert Coin";
+            this.gametitle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.gametitle.TextChanged += new System.EventHandler(this.startbutton_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.gametitle);
+            this.Controls.Add(this.startbutton);
+            this.Name = "Form1";
+            this.Text = "Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox gametitle;
+        private System.Windows.Forms.Button startbutton;
+    }
+}
+
---
+++ PracticeGame/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace PracticeGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+            
+        {
+            SoundPlayer sp2 = new SoundPlayer(Properties.Resources.Coin_Drop_Willem_Hunt_569197907);
+            sp2.Play();
+            Thread.Sleep(4000);
+
+            startbutton.BackColor = Color.DarkGreen;
+            startbutton.Text = "3";
+            Refresh();
+            Thread.Sleep(1000);
+
+            startbutton.Text = "2";
+            Refresh();
+            Thread.Sleep(1000);
+
+            startbutton.Text = "1";
+            Refresh();
+            Thread.Sleep(1000);
+
+            startbutton.Text = "Begin";
+            startbutton.BackColor = Color.DarkRed;
+            Refresh();
+            Thread.Sleep(1000);
+
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.Short_triumphal_fanfare_John_Stracke_815794903);
+            sp.Play();
+
+
+
+
+        }
+
+        private void startbutton_TextChanged(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
